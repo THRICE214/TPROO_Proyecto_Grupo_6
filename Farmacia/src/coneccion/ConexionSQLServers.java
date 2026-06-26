858d746b63416e958e8277b0f2df85af48f0868a
Alejandro <alejandronicolasgarcia01@gmail.com>
2026-06-25 21:12:02 -0500

    Agrego los nuevos cambios al proyecto, usando base de datos SQLServers y uniendo las ventanas, dandole tambien funcion al boton salir

--- Farmacia/src/coneccion/ConexionSQLServers.java
+++ Farmacia/src/coneccion/ConexionSQLServers.java
@@ -8,15 +8,12 @@ public class ConexionSQLServers {
     public static Connection getConexion() {
         Connection cnx = null;
         try {
-            // 1. Nuevo Driver de SQL Server
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             System.out.println("Driver correcto");
             
-            // 2. Nueva URL de conexión con el puerto 1433
-            // Nota: Se añaden encrypt y trustServerCertificate para evitar errores de conexión modernos
             String url = "jdbc:sqlserver://localhost:1433;databaseName=BD_Botica;encrypt=true;trustServerCertificate=true;";
-            String usuario = "sa"; // Asegúrate de que sea tu usuario de SQL Server
-            String contrasena = "NghAle676"; // Mantengo la contraseña que tenías en la captura
+            String usuario = "sa";
+            String contrasena = "NghAle676";
             
             cnx = DriverManager.getConnection(url, usuario, contrasena);
             System.out.println("Conexión correcta a SQL Server");
